f19a1ab50e2cf17cd34a1a472fad1c41a3144057
Tastylamborghini <arhamfaheem204@gmail.com>
2025-08-19 14:08:58 -0500

    REAL commit finally

---
+++ yuhmmy/src/App.js
@@ -0,0 +1,25 @@
+import logo from './RobloxLogo.webp';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          do NOT Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://www.roblox.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          screw React lol, play roblox instead haha
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
